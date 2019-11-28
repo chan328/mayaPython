045cffbd66b822512c2f43f38c8ccd3e616d0b66
chan <rkdcks020328@gmail.com>
2019-11-28 17:17:47 +0900

    os 모듈을 이용한 파일 추가, 검색

--- conLibrary/controllerLibrary.py
+++ conLibrary/controllerLibrary.py
@@ -16,3 +16,35 @@ def createDirectory(directory=DIRECTORY):
     """
     if not os.path.exists(directory):
         os.mkdir(directory)
+
+class ControllerLibrary(dict):
+    def save(self, name, directory=DIRECTORY):
+
+        createDirectory(directory)
+
+        path = os.path.join(directory, '%s.ma' % name)
+
+        cmds.file(rename=path)
+
+        if cmds.ls(selection=True):
+            cmds.file(force=True, type='mayaAscii', exportSelected=True)
+        else:
+            cmds.file(save=True, type='mayaAscii', force=True)
+
+    def find(self, directory=DIRECTORY):
+        if not os.path.exists(directory):
+            return
+
+        files = os.listdir(directory)
+        mayaFiles = [f for f in files if f.endswith('.ma')]
+        # 아래 코드와 동일 <List Comprehension>
+        # for f in files:
+        #   if f.endwiths('.ma') ~~
+
+        for ma in mayaFiles:
+            name, extention = os.path.splitext(ma)
+            path = os.path.join(directory, ma)
+
+            self[name] = path
+
+        pprint.pprint(self)
