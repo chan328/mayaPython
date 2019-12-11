5374de15c5c663b8dd11f8b2aa84d5c655b556ca
chan <rkdcks020328@gmail.com>
2019-12-11 16:18:27 +0900

    light manager updated
    light 생성 및 색 변경, 제거, refresh 기능 추가

--- LightManager/lightingManager.py
+++ LightManager/lightingManager.py
@@ -17,7 +17,20 @@ class LightManager(QtWidgets.QDialog):
     def __init__(self):
         super(LightManager, self).__init__()
         self.setWindowTitle('Lighting Manager')
+
         self.build_ui()
+        self.populate()
+
+    def populate(self):
+        while self.scrollLayout.count():
+            widget = self.scrollLayout.takeAt(0).widget()
+            if widget:
+                # takeAt = delete parent
+                widget.setVisible(False)
+                widget.deleteLater()
+
+        for light in pm.ls(type=["areaLight", "spotLight", "pointLight", "directionalLight", "volumeLight"]):
+            self.add_light(light)
 
     def build_ui(self):
         layout = QtWidgets.QGridLayout(self)
@@ -41,17 +54,33 @@ class LightManager(QtWidgets.QDialog):
         scroll_area.setWidget(scroll_widget)
         layout.addWidget(scroll_area, 1, 0, 1, 2)
 
+        refresh_button = QtWidgets.QPushButton('Refresh')
+        refresh_button.clicked.connect(self.populate)
+        layout.addWidget(refresh_button, 2, 1)
+
     def create_light(self):
         light_type = self.lightTypeCB.currentText()
         func = self.lightTypes[light_type]
 
         light = func()
+        self.add_light(light)
 
+    def add_light(self, light):
         widget = LightWidget(light)
         self.scrollLayout.addWidget(widget)
+        widget.on_solo.connect(self.on_solo)
+
+    def on_solo(self, value):
+        light_widgets = self.findChildren(LightWidget)
+        for widget in light_widgets:
+            if widget != self.sender():
+                widget.disable_light(value)
 
 
 class LightWidget(QtWidgets.QWidget):
+
+    on_solo = QtCore.Signal(bool)
+
     def __init__(self, light):
         super(LightWidget, self).__init__()
         if isinstance(light, str):
@@ -71,6 +100,62 @@ class LightWidget(QtWidgets.QWidget):
 
         layout.addWidget(self.name, 0, 0)
 
+        solo_button = QtWidgets.QPushButton('Solo')
+        solo_button.setCheckable(True)
+        solo_button.toggled.connect(lambda val: self.on_solo.emit(val))
+        layout.addWidget(solo_button, 0, 1)
+
+        delete_button = QtWidgets.QPushButton('X')
+        delete_button.clicked.connect(self.delete_light)
+        delete_button.setMaximumWidth(10)
+        layout.addWidget(delete_button, 0, 2)
+
+        intensity = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        intensity.setMinimum(1)
+        intensity.setMaximum(1000)
+        intensity.setValue(self.light.intensity.get())
+        intensity.valueChanged.connect(lambda val: self.light.intensity.set(val))
+        layout.addWidget(intensity, 1, 0, 1, 2)
+
+        self.color_button = QtWidgets.QPushButton()
+        self.color_button.setMaximumWidth(20)
+        self.color_button.setMaximumHeight(20)
+        self.set_button_color()
+        self.color_button.clicked.connect(self.set_color)
+        layout.addWidget(self.color_button, 1, 2)
+
+    def set_color(self):
+        light_color = self.light.color.get()
+        color = pm.colorEditor(rgbValue=light_color)
+
+        r, g, b, a = [float(c) for c in color.split()]
+        color = (r, g, b)
+
+        self.light.color.set(color)
+        self.set_button_color(color)
+
+    def set_button_color(self, color=None):
+        if not color:
+            color = self.light.color.get()
+
+        assert len(color) == 3, "You must provide a list of 3 colors"
+
+        # color = 0~255 float value
+        r, g, b = [c*255 for c in color]
+        # each value in color multiplying it by 255
+
+        self.color_button.setStyleSheet('background-color: rgba(%s, %s, %s, 1.0)' % (r, g, b))
+
+    def disable_light(self, value):
+        self.name.setChecked(not value)
+
+    def delete_light(self):
+        self.setParent(None)
+        # remove light manager
+        self.setVisible(False)
+        self.deleteLater()
+
+        pm.delete(self.light.getTransform())
 
 def showUI():
     ui = LightManager()
